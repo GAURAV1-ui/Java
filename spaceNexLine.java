9fcf80cf9b44fb52083bad1eb65e1f3b05eba5f0
Gaurav <iamgaurav201@gmail.com>
2022-09-06 15:22:39 +0530

    New commit

---
+++ spaceNexLine.java
@@ -0,0 +1,23 @@
+public class spaceNexLine {
+
+    public static void nextSpace(String s){
+        int start = 0;
+        int i =0;
+        s.trim();
+       while(i<s.length()){
+        if(s.charAt(i)!=){
+            i++;
+        }
+        else{
+            
+           System.out.println(s.substring(start,i));
+            start = i;
+            i++;
+        }
+       }
+    }
+    public static void main(String[] args) {
+        String s = " Hello world for new universe";
+        nextSpace(s);
+    }
+}
