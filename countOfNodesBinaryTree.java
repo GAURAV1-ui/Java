37ec5cd2304bfbda39f344c88e47e17437edeeef
Gaurav <iamgaurav201@gmail.com>
2022-12-05 12:09:21 +0530

    new commit

--- countOfNodesBinaryTree.java
+++ countOfNodesBinaryTree.java
@@ -34,11 +34,44 @@ public class countOfNodesBinaryTree {
 
         return leftNodes + rightNodes + 1;
        } 
+       public static int sumNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftNodes = sumNodes(root.left);
+        int rightNodes = sumNodes(root.right);
+
+        return leftNodes + rightNodes + root.data;
+       } 
+
+       public static int heightNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftHeight = heightNodes(root.left);
+        int rightHeight = heightNodes(root.right);
+        int ans = Math.max(leftHeight,rightHeight)+1;
+        return ans;
+       } 
+
+       public static int diameterNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftDia = diameterNodes(root.left);
+        int rightDia = diameterNodes(root.right);
+        int midDia = heightNodes(root.left) + heightNodes(root.right)+1 ;
+        return Math.max(midDia, Math.max(leftDia,rightDia));
+    
+       }
        public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        
+
         System.out.println(countNodes(root));
+        System.out.println(sumNodes(root));
+        System.out.println(heightNodes(root));
+        System.out.println(diameterNodes(root));
        } 
 }
