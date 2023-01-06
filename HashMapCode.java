ffd0b66421045d6e53fa8e05909fb2ec9e87637b
Gaurav <iamgaurav201@gmail.com>
2023-01-06 21:02:07 +0530

    Hashmap

---
+++ HashMapCode.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HashMap {
+    static class HashMap<K,V>{
+        private class Node{
+            K key;
+        V value;
+
+        }
+        
+        public Node(K key, V value) {
+
+        }
+    }
+}
