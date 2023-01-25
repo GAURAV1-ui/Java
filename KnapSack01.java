ef093bc615ec0da744e8dbdc076f2c41972ea017
Gaurav <iamgaurav201@gmail.com>
2023-01-26 01:13:35 +0530

    new commit

--- KnapSack01.java
+++ KnapSack01.java
@@ -1,3 +1,21 @@
 public class KnapSack01 {
-    
+
+    public static int KnapSack(int[] items, int [] val, int w, int n) {
+        if(n == 0 || w == 0){
+            return 0;
+        }
+
+        if(n) {
+            
+        }
+    }
+
+    public static void main(String args[]) {
+        int [] items= {};
+        int [] val = {};
+        int weight = 7;
+        int n = items.length;
+        
+        KnapSack(items, val, weight, n);
+    }
 }
