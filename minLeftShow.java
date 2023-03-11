e48165453ed0084ef36f4d5e61e51174d062b517
GAURAV <iamgaurav201@gmail.com>
2023-03-12 03:55:50 +0530

    new commit

---
+++ minLeftShow.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class minLeftShow {
+    
+        public static void main (String[] args){
+             int arr_size = 0;
+             Scanner sc = new Scanner(System.in);
+            if (sc.hasNextInt()) {
+                arr_size = sc.nextInt();
+            }
+      
+            int[] arr = new int[arr_size];
+      
+            for (int i = 0; i < arr_size; i++) {
+                if (sc.hasNextInt()) {
+                    arr[i] = sc.nextInt();
+                }
+                
+            }
+            for (int i = 0; i < arr_size; i++) {
+            if(arr[i] >= 24){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+             }
+        }
+}
