0baeb6c9f995a1cd31a1c56c1715225eaf77e6a0
GAURAV <iamgaurav201@gmail.com>
2023-03-30 11:42:54 +0530

    knapsack added

--- KnapSack01.java
+++ KnapSack01.java
@@ -1,37 +1,32 @@
 import java.util.*;
 
 public class KnapSack01 { 
-    public static int KnapSack(int[] wt, int [] val,int[][] t, int w, int n) {
+    public static int KnapSack(int[] wt, int [] val, int w, int n) {
         if(n == 0 || w == 0){
             return 0;
         }
-        for (int[] row : t){
-        Arrays.fill(row, -1);
-        }
+        // for (int[] row : t){
+        // Arrays.fill(row, -1);
+        // }
     
-    
-       if(t[n][w] != -1){
-        return t[n][w];
-       }
     
         if(wt[n-1] <=w){
-            return t[n][w] = Math.max(KnapSack(wt, val, t, w-wt[n-1], n-1),
-            KnapSack(wt, val, t, w, n-1));
+            return Math.max(val[n-1] + KnapSack(wt, val, w-wt[n-1], n-1),
+            KnapSack(wt, val, w, n-1));
         }
-        else if(wt[n-1]<w){
-            return t[n][w] = KnapSack(wt, val, t, w, n-1);
+        else{
+            return KnapSack(wt, val, w, n-1);
         }
+
     }
 
 
     public static void main(String args[]) {
-        int [] w= {1,2,3,1,2,8,5};
-        int [] val = {10,20,30,10,80,20,100};
-        int weight = 7;
-        int [][] t = new int [100][1000];
-        int n = w.length;
-        
-        int max = KnapSack(w, val, t,weight,n);
+        int val[] = new int[] { 60, 100, 120 };
+        int wt[] = new int[] { 10, 20, 30 };
+        int W = 50;
+        int n = wt.length;
+        int max = KnapSack(wt, val,W,n);
         System.out.println(max);
     }
 }
