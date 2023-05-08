ea0d389949873496786ff8a91a7e2d2bbcdb4db3
GAURAV <iamgaurav201@gmail.com>
2023-05-08 16:51:53 +0530

    new commit

---
+++ ninjaTraining.java
@@ -0,0 +1,11 @@
+public class ninjaTraining {
+
+    public static int NinjaTraining(int ) {
+            if(day == 0) {
+                
+            }
+    }
+    public static void main(String [] args) {
+        
+    }
+}
